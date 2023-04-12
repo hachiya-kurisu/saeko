d707337ac3d4f2922496a54d5607bcc463f7c2a2
蜂谷栗栖 <kurisu@blekksprut.net>
2023-04-12 15:32:06 +0900

    adjust snprintf

--- src/saeko.c
+++ src/saeko.c
@@ -111,7 +111,7 @@ void encode(char *src, char *dst) {
   for(; *s; s++) {
     if(skip[(int) *s]) snprintf(dst, 1, "%c", skip[(int) *s]), ++dst;
     else {
-      snprintf(dst, 16, "%%%02x", *s);
+      snprintf(dst, 3, "%%%02x", *s);
       while (*++dst);
     }
   }
