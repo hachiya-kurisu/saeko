370eeadb9bdde3fedfbb56c328e24f5b98e6bb75
蜂谷栗栖 <kurisu@blekksprut.net>
2025-01-20 16:51:09 +0900

    update defaults

--- config.def.h
+++ config.def.h
@@ -3,14 +3,12 @@ int wild = 1; // wild mode
 
 char *root = "/var/spartan"; // root path
 
-char text[LINE_MAX] = "text/gemini"; // plaintext mime type
-
 char *user = "spartan"; // setuid
 char *group = "spartan"; // setgid
 
 // domains to serve
 struct host hosts[] = {
-  { "localhost", "gmidocs" },
+  { "localhost", "." },
   { 0 }
 };
 
